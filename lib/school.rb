867abe05f90b7c173a2333ad75babcad58f43c09
Jen Lindner <jenlindner22@gmail.com>
2019-10-30 17:41:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
-# code here!
+class School
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def roster
+  roster = {}
+end
+
+def add_student(name, grade)
+@name = name
+@grade = grade
+roster 
+end
+end
